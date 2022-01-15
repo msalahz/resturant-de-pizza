90f067d8e9d0e2d6260a642a2d7beddca8dfc8a7
Mohammed Zaghloul <Mohammed.Zaghloul@genesys.com>
2022-01-15 21:21:13 +0200

    feat: add openSuccessSnackbar & openErrorSnackbar new functions to SnackbarContext

--- src/components/containers/SnackbarContainer.tsx
+++ src/components/containers/SnackbarContainer.tsx
@@ -1,3 +1,4 @@
+import _get from 'lodash/get'
 import _noop from 'lodash/noop'
 import IconButton from '@mui/material/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
@@ -18,6 +19,8 @@ export interface State extends Props {
 interface SnackbarContextProps {
   openSnackbar: (props: Props) => void
   closeSnackbar: () => void
+  openSuccessSnackbar: (body: string) => void
+  openErrorSnackbar: (error: unknown, title: string) => void
 }
 
 const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
@@ -28,6 +31,8 @@ const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) 
 const SnackbarContext = createContext<SnackbarContextProps>({
   openSnackbar: _noop,
   closeSnackbar: _noop,
+  openSuccessSnackbar: _noop,
+  openErrorSnackbar: _noop,
 })
 SnackbarContext.displayName = 'SnackbarContext'
 
@@ -44,7 +49,23 @@ function SnackbarProvider(props: { children: JSX.Element | JSX.Element[]; autoHi
   })
   const handleOpen = (props: Props) => setState({ ...state, open: true, ...props })
   const handleClose = () => setState({ ...state, open: false })
-  const snackbarContextProps: SnackbarContextProps = { openSnackbar: handleOpen, closeSnackbar: handleClose }
+  const handleSuccessOpen = (body: string) => handleOpen({ severity: 'success', body })
+
+  const handleErrorOpen = (error: unknown, title: string) => {
+    const msg = _get(error, 'message', undefined)
+    handleOpen({
+      severity: 'error',
+      title: msg ? title : undefined,
+      body: msg ? msg : title,
+    })
+  }
+
+  const snackbarContextProps: SnackbarContextProps = {
+    openSnackbar: handleOpen,
+    closeSnackbar: handleClose,
+    openSuccessSnackbar: handleSuccessOpen,
+    openErrorSnackbar: handleErrorOpen,
+  }
 
   return (
     <SnackbarContext.Provider value={snackbarContextProps}>
